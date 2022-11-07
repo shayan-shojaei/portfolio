5cf5810e462413df91577434218c569be955ccad
Shayan Shojaei <68788931+shayan-shojaei@users.noreply.github.com>
2022-11-07 10:29:21 +0330

    update: resources

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,52 +1,52 @@
-import './Home.scss';
-import { GitHub, LinkedIn, Email, Twitter } from '../Icons/Social';
+import "./Home.scss";
+import { GitHub, LinkedIn, Email, Twitter } from "../Icons/Social";
 
 export default function Home() {
-	return (
-		<div className="page">
-			<div className="page-content">
-				<span className="page-route">home/</span>
-				<h1>
-					hey, i'm <strong>shayan</strong>!
-				</h1>
-				<p>
-					I'm a software developer and have worked in multiple fields such as
-					Web and Mobile software development. I've worked with several
-					languages and consider myself proficient in
-					<strong> JavaScript, Python and Java</strong>, etc. I'm currently
-					studying Computer Science and am highly interested in Deep Learning
-					research, concepts and techniques.
-				</p>
-				<section className="socials">
-					<SocialItem
-						label="My Github Profile"
-						url="https://github.com/shayan-shojaei"
-						icon={<GitHub />}
-					/>
-					<SocialItem
-						label="My LinkedIn Profile"
-						url="https://linkedin.com/in/shayan-shojaei"
-						icon={<LinkedIn />}
-					/>
-					<SocialItem
-						label="shayanshojaei.2002@gmail.com"
-						url="mailto:shayanshojaei.2002@gmail.com"
-						icon={<Email />}
-					/>
-					<SocialItem
-						label="My Twitter Account"
-						url="https://twitter.com/shayan_shojaei"
-						icon={<Twitter />}
-					/>
-				</section>
-			</div>
-		</div>
-	);
+  return (
+    <div className="page">
+      <div className="page-content">
+        <span className="page-route">home/</span>
+        <h1>
+          Hey, I'm <strong>Shayan</strong>
+        </h1>
+        <p>
+          I'm a software engineer and have worked as a back-end and front-end
+          developer. I've worked with several languages and consider myself
+          proficient in JavaScript, Typescript, Python, Java, etc. and I
+          currently work as a NodeJS developer. I study laso Computer Science
+          and am highly interested in Deep Learning research, concepts and
+          techniques.
+        </p>
+        <section className="socials">
+          <SocialItem
+            label="My Github Profile"
+            url="https://github.com/shayan-shojaei"
+            icon={<GitHub />}
+          />
+          <SocialItem
+            label="My LinkedIn Profile"
+            url="https://linkedin.com/in/shayan-shojaei"
+            icon={<LinkedIn />}
+          />
+          <SocialItem
+            label="shayanshojaei.2002@gmail.com"
+            url="mailto:shayanshojaei.2002@gmail.com"
+            icon={<Email />}
+          />
+          <SocialItem
+            label="My Twitter Account"
+            url="https://twitter.com/shayan_shojaei"
+            icon={<Twitter />}
+          />
+        </section>
+      </div>
+    </div>
+  );
 }
 
 const SocialItem = (props: { label: string; url: string; icon: any }) => (
-	<a href={props.url} target="_blank" rel="noreferrer">
-		{props.icon}
-		<span>{props.label}</span>
-	</a>
+  <a href={props.url} target="_blank" rel="noreferrer">
+    {props.icon}
+    <span>{props.label}</span>
+  </a>
 );
