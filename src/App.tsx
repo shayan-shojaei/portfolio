14b5a6e64e1b2faf9ea89f01d643b457269312e7
Shayan Shojaei <shayanshojaei.2002@gmail.com>
2022-11-07 21:14:29 +0330

    fix: router type

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss';
 import Nav, { NavRoute } from './Components/Nav/Nav';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Projects from './Components/Projects/Projects';
 import Skills from './Components/Skills/Skills';
